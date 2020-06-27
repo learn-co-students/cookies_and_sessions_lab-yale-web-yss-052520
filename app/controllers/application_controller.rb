06d3d75150b066fcb0e9948e652d0043215cf324
ZIming Mao <ziming.mao@yale.edu>
2020-06-27 09:25:15 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :cart
+  def cart
+    
+    session[:cart] ||= []
+    # Set session[:cart] equals to [] if it is not defined
+  end
 end
