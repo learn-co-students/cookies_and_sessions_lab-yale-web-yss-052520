f589cb559488920c968b15ed3a830fbdebdffd5a
Drew Beckmen <drew.beckmen@yale.edu>
2020-06-28 15:42:54 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # makes this controller method available to the view 
+  # can use it in products/index.html.erb 
+  helper_method :cart
+
+  def cart 
+    # using Ruby's conditional assignment operator!
+    session[:cart] ||= []
+  end 
 end
